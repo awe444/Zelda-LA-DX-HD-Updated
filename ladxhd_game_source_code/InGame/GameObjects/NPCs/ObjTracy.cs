bee7b81af6663bcecda2bfabf42662b8c7d85577
Scott Zimmerman <bighead.0@gmail.com>
2025-09-07 11:38:47 -0400

    Fix resetting Crazy Tracy message state when leaving and base price on actual kills.
    While it was never a huge deal, if you talk to tracy, see her initial message, then leave, next time you enter the building, she will pick up where she left off. So if you have a potion, talk to her, leave, get hit and lose your potion, then go back and talk to her, she will say "But I'm not selling it to you" even when you need a potion. This fixes that, and bases the price on real kills instead of piece of power counter, which is a slight accuracy increase. Like hitting piece of power at 44 kills resets to 0, so there is two continuous kill states where the price is lower and constantly fluctuates.

--- ladxhd_game_source_code/InGame/GameObjects/NPCs/ObjTracy.cs
+++ ladxhd_game_source_code/InGame/GameObjects/NPCs/ObjTracy.cs
@@ -21,15 +21,23 @@ namespace ProjectZ.InGame.GameObjects.NPCs
 
         public ObjTracy(Map.Map map, int posX, int posY) : base(map)
         {
-            Animator = AnimatorSaveLoad.LoadAnimator("NPCs/npc_tracy");
-            Animator.Play("idle");
+            // Tracy will retain her "in-between" state between messages even when leaving the
+            // building and after reloading a save file, so clear upon entering the building.
+            Game1.GameManager.SaveManager.SetString("npc_tracy", "0");
 
-            // 28, 42 or 7
-            var price = Game1.GameManager.PieceOfPowerCount % 2 == 0 ? 0 : 1;
+            // Even kills > 28 rupees
+            // Odd kills  > 42 rupees
+            var price = Game1.GameManager.KillCount % 2 == 0 ? 0 : 1;
+
+            // Bought 8 potions > 7 rupees
             if (Game1.GameManager.SaveManager.GetInt("npc_tracy", 0) == 8)
                 price = 2;
-            
+
+            // The actual price is set in "scripts.zScripts".
             Game1.GameManager.SaveManager.SetString("npc_tracy_price", price.ToString());
+
+            Animator = AnimatorSaveLoad.LoadAnimator("NPCs/npc_tracy");
+            Animator.Play("idle");
 
             EntityPosition = new CPosition(posX + 8, posY + 16, 0);
             EntitySize = new Rectangle(-8, -16, 16, 16);
