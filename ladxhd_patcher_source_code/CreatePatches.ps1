77e205a4f6979fbd672c53a94c0760b32a18a3e0
Scott Zimmerman <bighead.0@gmail.com>
2025-11-28 02:00:14 -0500

    Set to v1.5.0.beta2.

--- ladxhd_patcher_source_code/CreatePatches.ps1
+++ ladxhd_patcher_source_code/CreatePatches.ps1
@@ -55,7 +55,7 @@
 
 $OldGamePath = "C:\Users\Bighead\source\repos\Zelda-LA-DX-HD_Stuff\original"
 $NewGamePath = "C:\Users\Bighead\source\repos\Zelda-LA-DX-HD_Stuff\updated"
-$GameVersion = "1.5.0.b1"
+$GameVersion = "1.5.0.b2"
 
 #========================================================================================================================================
 # SETUP XDELTA & OUTPUTS
