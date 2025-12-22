0e063d992042d41e9f0ef040d3bf251e70b80ab7
Scott Zimmerman <bighead.0@gmail.com>
2025-12-22 04:12:38 -0500

    Implement complementary one-way collision on 2D objects.
    This is to go with the "pixel stairs" objects. When reaching the top of the object, it makes the object act as if it is flat.

--- ladxhd_game_source_code/InGame/GameObjects/GameObjectTemplates.cs
+++ ladxhd_game_source_code/InGame/GameObjects/GameObjectTemplates.cs
@@ -82,6 +82,7 @@ namespace ProjectZ.InGame.GameObjects
             // One-way collision boxes.
             ObjectTemplates.Add("oneWayBridge2", new GameObjectTemplate(typeof(ObjColliderOneWay), new object[] { new Rectangle(15, 0, 1, 16), Values.CollisionTypes.Normal, 2 }));
             ObjectTemplates.Add("oneWayBridge0", new GameObjectTemplate(typeof(ObjColliderOneWay), new object[] { new Rectangle(0, 0, 1, 16), Values.CollisionTypes.Normal, 0 }));
+            ObjectTemplates.Add("oneWayFlatTop", new GameObjectTemplate(typeof(ObjColliderOneWay), new object[] { new Rectangle(0, 0, 16, 1), Values.CollisionTypes.Normal, 3 }));
 
             // Unique collision boxes.
             ObjectTemplates.Add("2d_rounded", new GameObjectTemplate(typeof(ObjCollider), new object[] { true, specColliderColor, Values.CollisionTypes.Normal, new[] { new Rectangle(4, 0, 8, 1), new Rectangle(3, 1, 10, 1), new Rectangle(2, 2, 12, 1), new Rectangle(1, 3, 14, 1), new Rectangle(0, 4, 16, 12) }}));
