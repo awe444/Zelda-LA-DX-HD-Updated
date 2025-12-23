12ac949cf159ed53d975738ecd87230fa7c2f225
Scott Zimmerman <bighead.0@gmail.com>
2025-12-23 08:17:48 -0500

    Respawned Bombites reclose doors in dungeon 3.
    There's a room where 3 Bombites and two Zols spawn that closes the doors behind the player. When this room is cleared and the player leaves it, the Bombites respawn. This commit makes it so that the enemy trigger in the room resets and recloses the doors.

--- ladxhd_game_source_code/InGame/GameObjects/Things/ObjEnemyTrigger.cs
+++ ladxhd_game_source_code/InGame/GameObjects/Things/ObjEnemyTrigger.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using ProjectZ.InGame.GameObjects.Base;
 using ProjectZ.InGame.GameObjects.Base.CObjects;
 using ProjectZ.InGame.GameObjects.Base.Components;
+using ProjectZ.InGame.Map;
 using ProjectZ.InGame.Things;
 
 namespace ProjectZ.InGame.GameObjects.Things
@@ -16,8 +17,10 @@ namespace ProjectZ.InGame.GameObjects.Things
         private int _posX;
         private int _posY;
         private bool _enemiesAlive;
-        private bool _init;
+        private bool _findEnemies;
         private bool _respawn;
+
+        private float _recheckTimer;
 
         public ObjEnemyTrigger() : base("editor enemy trigger") { }
 
@@ -37,37 +40,55 @@ namespace ProjectZ.InGame.GameObjects.Things
             _triggerKey = triggerKey;
             _triggerField = map.GetField(posX, posY);
             _respawn = respawn;
+            _findEnemies = true;
 
             AddComponent(UpdateComponent.Index, new UpdateComponent(Update));
         }
 
         private void Update()
         {
-            // this gets called the first time update is run so it can capture enemies spawned by an ObjObjectSpawner
-            if (!_init)
+            // If respawn is set we need a way to check for new enemies.
+            if (_respawn)
             {
-                _init = true;
+                // A timer is used to recheck for enemies so it's not constantly rechecking.
+                _recheckTimer += Game1.DeltaTime;
 
-                // get the enemies the object should watch over
+                if (_recheckTimer > 200)
+                {
+                    _findEnemies = true;
+                    _recheckTimer = 0;
+
+                    // A previous loop iteration will note if enemies are alive.
+                    if (_enemiesAlive & _triggerField.Contains(MapManager.ObjLink.EntityPosition.Position));
+                        Game1.GameManager.SaveManager.SetString(_triggerKey, "0");
+                }
+            }
+            // Find any enemies in the current field.
+            if (_findEnemies)
+            {
                 Map.Objects.GetGameObjectsWithTag(EnemyTriggerList, Values.GameObjectTag.Enemy,
                     _triggerField.X, _triggerField.Y, _triggerField.Width, _triggerField.Height);
+                _findEnemies = false;
             }
+            // Assume all enemies are defeated.
             _enemiesAlive = false;
 
-            // check if the enemies where deleted from the map
+            // Check if no more enemies remain in the field.
             foreach (var gameObject in EnemyTriggerList)
                 if (gameObject.Map != null)
                     _enemiesAlive = true;
 
+            // If there are still enemies rerun the loop.
             if (_enemiesAlive)
                 return;
 
+            // All enemies were defeated so set the key.
             Game1.GameManager.SaveManager.SetString(_triggerKey, "1");
 
-            //RemoveComponent(UpdateComponent.Index);
-            // remove the object
+            // Remove the enemy trigger from the map.
             Map.Objects.DeleteObjects.Add(this);
 
+            // If it's a persistent enemy trigger respawn a new one.
             if (_respawn)
                 Map.Objects.SpawnObject(new ObjEnemyTrigger(Map, _posX, _posY, _triggerKey, _respawn));
         }
