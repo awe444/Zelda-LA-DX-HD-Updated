3cd762d9c77dee034cbff283ec2d83c5bae745df
Scott Zimmerman <bighead.0@gmail.com>
2025-12-26 02:15:40 -0500

    Fix movement speed modifier when X velocity is 0 on 2D maps.
    When on 2D maps, pressing a vertical direction would apply the movement speed modifier to Link's horizontal movement if set above 0.

--- ladxhd_game_source_code/InGame/GameObjects/ObjLink2d.cs
+++ ladxhd_game_source_code/InGame/GameObjects/ObjLink2d.cs
@@ -425,8 +425,16 @@ namespace ProjectZ.InGame.GameObjects
                 }
                 if (_body.IsGrounded && CurrentState != State.Hookshot && !_hookshotPull)
                 {
+                    // Default the added speed to zero.
+                    float addSpeed = 0;
+
                     // If the modifier to add movement speed is used then apply it to 2D walking speed.
-                    var addSpeed = walkVelocity.X > 0 ? GameSettings.MoveSpeedAdded : -GameSettings.MoveSpeedAdded;
+                    if (walkVelocity.X != 0)
+                    {
+                        addSpeed = walkVelocity.X > 0
+                            ? GameSettings.MoveSpeedAdded
+                            : -GameSettings.MoveSpeedAdded;
+                    }
                     _moveVector2D = new Vector2(walkVelocity.X + addSpeed, 0);
                     _lastMoveVelocity = _moveVector2D;
                 }
