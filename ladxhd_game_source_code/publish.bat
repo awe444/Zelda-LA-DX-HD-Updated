4c405aa3066d6a4f766ba391e111bb43719cf532
Scott Zimmerman <bighead.0@gmail.com>
2026-03-03 16:32:02 -0500

    Update publish profiles and fix various other build issues.
    This took me on quite a ride. I didn't realize it when building with visual studio and testing, but newer versions of MonoGame change some things up. It's no longer possible to build and get the exact same type of output that I used to with older MonoGame, but it is possible to get close. The downside is the executable is a lot bigger than it used to be. Newer versions of MonoGame include a bunch of DLL files that were not included with previous versions. Since this wasn't possible to overcome, I figured I would just pack in the necessary .NET files as well so the game no longer requires the .NET 8.0 runtime. At least I think, I haven't tested it yet.
    
    Currently this just fixes the "publish.bat" script to output both a Windows Direct-X and an OpenGL build to a "~Publish" folder in the same director as the batch file. I still need to update the migration tool to be able to build these as well. I just wanted to get this working before that though, so I will update the migration tool in the next commit.

--- ladxhd_game_source_code/publish.bat
+++ ladxhd_game_source_code/publish.bat
@@ -1,1 +1,11 @@
-dotnet publish -c Release -p:"PublishProfile=FolderProfile"
+@echo off
+echo Building Windows DirectX...
+dotnet publish ProjectZ.Desktop\ProjectZ.Desktop.csproj -c Release -f net8.0-windows -r win-x64 -p:PublishProfile=FolderProfile_DX
+if %errorlevel% neq 0 ( echo DX build failed! & pause & exit /b 1 )
+
+echo Building Windows OpenGL...
+dotnet publish ProjectZ.Desktop\ProjectZ.Desktop.csproj -c Release -f net8.0 -r win-x64 -p:PublishProfile=FolderProfile_GL
+if %errorlevel% neq 0 ( echo GL build failed! & pause & exit /b 1 )
+
+echo Done! Builds are in the Publish folder.
+pause
